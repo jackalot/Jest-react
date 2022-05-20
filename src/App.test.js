a18120f7730fda6f3a518c1686f6c37827b9adf1
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-20 08:59:37 -0400

    imported userEvent to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 import React from "react";
 
 describe("App component", () => {
