f5cc2a4d0ac6fe38532bb68bb5d24d521ed07a71
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-20 08:06:52 -0400

    changed the App component to a arrow function component with explicit return

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
 import React from 'react';
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+
+const App = () => <h1>Our First Test </h1>;
 
 export default App;
