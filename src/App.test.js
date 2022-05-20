8460e7f474a511b766766f988d97103902422749
jackalotUVM <gonzalezc9999@gmail.com>
2022-05-20 09:28:33 -0400

    added new tests to accomodate our current App.js, also tests the button

--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,18 @@ import userEvent from '@testing-library/user-event';
 import React from "react";
 
 describe("App component", () => {
-  it("renders correct heading", () => {
-    const { getByRole } = render(<App />);
-    expect(getByRole("heading").textContent).toMatch(/our first test/i);
+  it("remders, magnificent monkeys", () => {
+    // since screen does not have the container propery, we'll destructure render to obtain container for this test
+    const { container } = render(<App></App>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after button click", () => {
+    render(<App></App>);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    userEvent.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
   });
 });
